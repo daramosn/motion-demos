ef55055af8ad3f5a83a4b26f1644027d3ebc33ba
daramosn <daramosn@unal.edu.co>
2024-06-23 22:02:34 -0500

    feat: add LenisScroll component

---
+++ src/components/LenisScroll.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import Lenis from 'lenis'
+import { onMounted, onUnmounted } from 'vue'
+
+const lenis = new Lenis()
+
+onMounted(() => {
+  function raf(time: number) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+})
+
+onUnmounted(() => {
+  if (lenis) lenis.destroy()
+})
+</script>
+
+<style>
+html.lenis,
+html.lenis body {
+  height: auto;
+}
+
+.lenis.lenis-smooth {
+  scroll-behavior: auto !important;
+}
+*/ .lenis.lenis-smooth [data-lenis-prevent] {
+  overscroll-behavior: contain;
+}
+
+.lenis.lenis-stopped {
+  overflow: hidden;
+}
+
+.lenis.lenis-smooth iframe {
+  pointer-events: none;
+}
+</style>
